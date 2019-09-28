2af70956c5b1833a8532201a35785f42b494c6b7
takareinhard <hokke.takara@gmail.com>
2019-09-28 16:36:19 +0900

    本番環境のみでBasic認証をするようにしよう

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,9 @@
 	
 server '52.69.27.167', user: 'ec2-user', roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
