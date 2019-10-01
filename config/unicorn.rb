4143707b0236bc059a3958499f540bd2015fcdf2
takareinhard <hokke.takara@gmail.com>
2019-10-01 21:18:22 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,11 @@
-RAILS_ROOT = File.expand_path('../../../', __FILE__)
+
 
 worker_processes 1
+RAILS_ROOT = File.expand_path('../../../', __FILE__)
 # currentを指定
 # # 実行したファイルの場所からrailsのrootパスを見つける
 # RAILS_ROOT = File.expand_path('../../', __FILE__)
-ENV['BUNDLE_GEMFILE'] = RAILS_ROOT + "/Gemfile"
+# ENV['BUNDLE_GEMFILE'] = RAILS_ROOT + "/Gemfile"
 # ここは、RIALS_ROOTが記載されていれば、明示的にかかなくても大丈夫です。
 # 自分が使っている環境でも、下の記載なしにちゃんと動いてますね。
 working_directory RAILS_ROOT
