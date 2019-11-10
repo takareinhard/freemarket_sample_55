bb626b7a6f3f34acb98d1f43180c8178068dc57e
takareinhard <hokke.takara@gmail.com>
2019-11-10 18:27:18 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,33 @@ Rails.application.routes.draw do
   root to: "products#index"
   resources :products
   
-
   # devise_for :users, controllers: { registrations: 'users/registrations' }
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
-  root to: "categories#index"
-  resources :categories
+
+  resources :signup ,only: [:index,:create] do
+      collection do
+        get 'registration'                               # 図①  
+        post 'registration' => 'signup#first_validation' # 図❶
+        #中略
+        #電話番号入力フォーム
+        get 'sms_authentication' 
+        #入力された番号へのSMS送信、送信した文字列の保持
+        post 'sms_authentication' => 'signup#sms_post'
+        #受信した値の入力フォーム
+        get 'sms_confirmation' 
+        #値の照合
+        post 'sms_confirmation' => 'signup#sms_check'
+        #一致したら下のパスに飛んで登録画面が続く
+        get 'address'                                    # 図④
+        post 'address' => 'signup#second_validation'     # 図❹
+        get 'credit_card'
+        post 'credit_card' => 'signup#create' 
+        get 'done'                                       # 図⑥
+      end
+    end
 
   resources :card, only: [:new, :show] do
     collection do
@@ -21,7 +39,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :products, only: [:new, :create] do
+  resources :products, only: [:new, :create, :index] do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
@@ -30,26 +48,10 @@ Rails.application.routes.draw do
     end
   end
 
-
-  #新規会員登録のページ達
-  get "member_information_input" => 'categories#member_information_input' #会員情報入力
-  get "sms_authentication" => 'users#sms_authentication' #電話番号認証
-  get "profile" => 'categories#profile' #お届け先住所入力
-  get "payment" => 'categories#payment' #支払い方法
-  get "member_finish" => 'categories#member_finish' #完了
   resources :categories, only:[:show, :index] do
     collection do
     end
   end
-
-  # resources :card, only: [:new, :show] do
-  #   collection do
-  #     post 'show', to: 'card#show'
-  #     post 'pay', to: 'card#pay'
-  #     post 'delete', to: 'card#delete'
-  #   end
-  # end
-  
 
   #ログインページ
   get "old_member_registration" => 'categories#old_member_registration' #既存メンバーのログインページ
