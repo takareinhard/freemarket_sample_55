70f1b9f1eae13f3def426959c394e2a95afd5de6
tomo1224410 <sadfgwqerty@gmail.com>
2019-11-10 17:12:01 +0900

    edit csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -334,3 +334,8 @@ others_8 = others.children.create(name: "事務/店舗用品")
 others_8.children.create([{name: "オフィス用品一般"},{name: "オフィス家具"},{name: "店舗用品"},{name: "OA機器"},{name: "ラッピング/包装"},{name: "その他"}])
 others_9 = others.children.create(name: "その他")
 others_9.children.create([{name: "すべて"}])
+
+#都道府県
+CSV.foreach('db/prefecture.csv') do |row|
+  Prefecture.find_or_create_by(prefecture: row[0])
+end
