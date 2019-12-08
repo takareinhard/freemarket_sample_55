30a5583897964ba8c6f5355d28f268f060171ecc
takareinhard <hokke.takara@gmail.com>
2019-12-08 14:50:08 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,14 +19,10 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create!(name: products_params[:name], detail: products_params[:detail], detail: products_params[:detail], condition: products_params[:condition], postage_payer: products_params[:postage_payer],
      shipping_method: products_params[:shipping_method],prefecture_id: products_params[:prefecture_id],shipping_days: products_params[:shipping_days],price: products_params[:price],
-<<<<<<< Updated upstream
-    user_id: current_user.id, category_id: params[:category_id], brand_id: 1, shipping_area: 10)
-=======
-    user_id: current_user.id, category_id: params[:category_id], image: params[:image])
-    @pruduct_image = ProductImage.new   
+    user_id: current_user.id, category_id: params[:category_id],brand_id: 1,shipping_area: 10)
+    @pruduct_image = ProductImage.create!
     binding.pry
     @product.save
->>>>>>> Stashed changes
     redirect_to root_path, notice: "出品が完了しました"
   end
 
